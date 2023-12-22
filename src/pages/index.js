efb31cf8101f507fe6752139769414347b24c539
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-12-22 18:08:32 +0100

    done

---
+++ src/pages/index.js
@@ -0,0 +1,138 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="bg-[#f3332f]">
+        <Image src={"/logo.png"} width={80} height={80} />
+
+        <div className="sm:flex py-[40px] ">
+          <div className="s">
+            <Image src={"/logo.png"} width={500} height={500} />
+          </div>
+          <div className="sm:mt-[100px] mx-[60px] sm:mx-[0px]">
+            <h1>THE DOG COIN OF THE PEOPLE</h1>
+            <p>
+              Missed out on $BONK? Here's your chance with generational wealth
+              opportunity with twice the better on $BONK2.0 !
+            </p>
+            <p>Gh9AcyeZezm5DBvVpG9eSwN1pArtFGNU9q7RzLKkkQ8n</p>
+
+            <div className="flex text-white  space-x-4 pt-3">
+              <button className="bg-white text-[#f28b8c] px-3 py-3 hover:bg-[#524c47] hover:text-white">
+                Buy here
+              </button>
+              <button className="bg-white text-[#f28b8c] px-4 py-3 hover:bg-[#524c47] hover:text-white">
+                Chart
+              </button>
+              <button className="bg-white text-[#f28b8c] px-3 py-3 hover:bg-[#524c47] hover:text-white">
+                Telegram
+              </button>
+              <button className="bg-white text-[#f28b8c] px-9 py-3 hover:bg-[#524c47] hover:text-white">
+                X
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* second secction */}
+      <div className="bg-white text-center text-[#524c47] py-[100px]">
+        <div>
+          <button className="bg-[#f3332f] px-5 py-5 text-white">
+            OUR MISSION
+          </button>
+
+          <p className="text-center pt-5">
+            Bonk 2.0 is dedicated to becoming the premier Web3 community on the
+            Solana blockchain. Our mission is to foster inclusivity, innovation,
+            and decentralized collaboration. We aim to create a vibrant space
+            where creators, developers, and enthusiasts converge to push the
+            boundaries of what's possible. Through transparent governance and
+            community-driven initiatives, Bonk 2.0 seeks to set the standard for
+            excellence in the Solana ecosystem. We are committed to providing a
+            dynamic platform that empowers individuals, fosters creativity, and
+            propels us to be the best Web3 community on the Solana chain. Join
+            us in shaping the future of decentralized collaboration.
+          </p>
+        </div>
+      </div>
+
+      {/* third section */}
+      <div className="bg-[#f3332f] text-center text-white py-[100px]">
+        <button className="bg-white  px-5 py-5 text-[#f3332f]">
+          TOKENOMICS
+        </button>
+
+        <div className="flex space-x-8 justify-center pt-[30px]">
+          <div>
+            <p>100,000,000</p>
+            <p>Total Supply</p>
+          </div>
+
+          <div>
+            <p>0/0</p>
+            <p>Tax</p>
+          </div>
+        </div>
+      </div>
+
+      {/* forth section */}
+      <div className="bg-white  text-[#f3332f] py-[120px]">
+        <div className="grid justify-items-center">
+          <button className="bg-[#f3332f]  px-5 py-5 text-white">
+            ROADMAP
+          </button>
+        </div>
+
+        <div>
+          {/* content 1 */}
+          <div className="px-[60px] sm:mx-[100px] pt-[30px] text-balance justify-items-center grid  grid-cols-2 sm:grid sm:grid-cols-4 space-y-[30px] sm:space-y-[0px] space-x-[20px]  sm:space-x-[70px]">
+            <div className="pt-[30px] sm:pt-[0px] pl-[20px] sm:pl-[20px]">
+              <h2>PHASE 1</h2>
+
+              <ul className="list-disc text-[#524c47]">
+                <li> Token launch</li>
+                <li> Website and social medias</li>
+                <li>Bonk meme generator</li>
+              </ul>
+            </div>
+
+            <div>
+              <h2>PHASE 1</h2>
+
+              <ul className="list-disc text-[#524c47]">
+                <li> Token launch</li>
+                <li> Website and social medias</li>
+                <li>Bonk meme generator</li>
+              </ul>
+            </div>
+
+            <div>
+              <h2>PHASE 1</h2>
+
+              <ul className="list-disc text-[#524c47]">
+                <li> Token launch</li>
+                <li> Website and social medias</li>
+                <li>Bonk meme generator</li>
+              </ul>
+            </div>
+
+            <div>
+              <h2>PHASE 1</h2>
+
+              <ul className="list-disc text-[#524c47]">
+                <li> Token launch</li>
+                <li> Website and social medias</li>
+                <li>Bonk meme generator</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
