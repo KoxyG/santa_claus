050c61730c283626dd0ccaaed812b393cdeffe19
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-12-23 02:07:00 +0100

    made changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,140 +1,104 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 
-const poppins = Poppins({weight: ['200', '600'],  subsets: ["latin"] });
+const poppins = Poppins({ weight: ["200", "600"], subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="">
-      <div className="bg-[#f3332f]">
-        <Image src={"/logo.png"} width={60} alt="logo" height={60} />
+      <div className="relative flex h-full w-full items-center justify-center">
+        <Image
+          src={"/santa.jpg"}
+          width={200}
+          height={100}
+          alt="image"
+          className="filter brightness-50 shadow-lg' block h-full w-full object-cover"
+        />
+        
+        <div className=" absolute">
+        {/* <Image src={"/logo.png"} width={60} alt="logo" height={60} /> */}
 
         <div className="sm:flex py-[40px] ">
-          <div className="">
-            <Image src={"/logo.png"} alt="logo" width={700} height={700} />
-          </div>
-          <div className="sm:mt-[100px]  mx-[60px] sm:mx-[0px]  text-white">
-            <h1 className="text-white pb-[30px] text-4xl font-bold sm:leading-loose">THE SANTA COIN OF THE PEOPLE</h1>
+          
+          <div className="sm:mt-[100px]  mx-[60px] text-center sm:mx-[0px]  text-white">
+            <h1 className="text-white pb-[30px] text-4xl sm:text-6xl font-bold sm:leading-loose">
+              SANTA COIN FOR THE PEOPLE
+            </h1>
             {/* eslint-disable react/no-unescaped-entities */}
-            <p className="text-white sm:text-[20px]">
-              Missed out on $BONK? Here's your chance with generational wealth <br />{" "}
-              opportunity with twice the better on $BONK2.0 
+            {/* <p className="text-white sm:text-[20px]">
+              Missed out on $BONK? Here's your chance with generational wealth{" "}
+              <br /> opportunity with twice the better on $BONK2.0
             </p>
-            {/* eslint-disable react/no-unescaped-entities */}
-            <p className="text-white text-[10px] pt-[10px]  sm:text-[12px]">Gh9AcyeZezm5DBvVpG9eSwN1pArtFGNU9q7RzLKkkQ8n</p>
+            eslint-disable react/no-unescaped-entities */}
+            {/* <p className="text-white text-[10px] pt-[10px]  sm:text-[12px]">
+              Gh9AcyeZezm5DBvVpG9eSwN1pArtFGNU9q7RzLKkkQ8n
+            </p> */}
+            <p className="text-white sm:text-[20px]">
+            $Santa Coin Contract: 0xaDBA987955Eac146f1983062100046be46e632fA
+            </p>
 
-            <div className="flex text-white  gap-4 pt-3">
+            <div className="flex justify-center text-white  gap-4 pt-3">
+              {/* <Link  href="https://t.me/santaclausonavax" >
               <button className="bg-white   text-sm sm:text-lg text-[#f28b8c]  px-2 py-2  sm:px-3 sm:py-3 hover:bg-[#524c47] hover:text-white">
                 Buy here
               </button>
+              </Link>
               <button className="bg-white  text-sm sm:text-lg text-[#f28b8c] px-3 py-2  sm:px-4 sm:py-3 hover:bg-[#524c47] hover:text-white">
                 Chart
-              </button>
-              <button className="bg-white  text-sm sm:text-lg text-[#f28b8c] px-2 py-2 sm:px-3 sm:py-3 hover:bg-[#524c47] hover:text-white">
+              </button> */}
+              <Link  href="https://t.me/santaclausonavax" >
+              <button className="bg-white  text-sm sm:text-lg text-[#f28b8c] px-3 py-3 hover:bg-[#524c47] hover:text-white">
                 Telegram
               </button>
-              <button className="bg-white  text-sm sm:text-lg text-[#f28b8c] px-6 py-2  sm:px-9 sm:py-3 hover:bg-[#524c47] hover:text-white">
+              </Link>
+
+              <Link href="https://x.com/santaclaus_avax?t=a1Ni1QvGshnnSSHI0wBf8w&s=08">
+              <button className="bg-white  text-sm sm:text-lg text-[#f28b8c] px-9 py-3 hover:bg-[#524c47] hover:text-white">
                 X
               </button>
+              </Link>
             </div>
           </div>
+        </div>
+      </div>
+        </div>
+      <div className="bg-white">
+        {/* <Image src={"/logo.png"} width={60} alt="logo" height={60} /> */}
+
+        <div className="grid justify-center align-items-center pb-[100px]">
+          <div className="">
+            <Image src={"/logo.png"} alt="logo" width={700} height={700} />
+          </div>
+         
+            
+          
+            
+            <p className="mx-[60px] text-[#f3332f] text-center pt-5">
+            It’s finally the time of the year where we gift our loved ones to
+            celebrate the birth of Jesus Christ. This practice was invented by
+            Santa Claus who is also known as the father of Christmas. Santa
+            Claus on avax is a meme token without any financial return/reward
+            created to for fun to celebrate the birth of Jesus Christ and also
+            to Celebrate the father of Christmas Santa.
+          </p>
+            
+         
         </div>
       </div>
 
       {/* second secction */}
-      <div className="bg-white  text-center text-[#524c47] py-[100px]">
+      <div className="bg-[#f3332f]  text-center text-[#524c47] py-[100px]">
         <div>
-          <button className="bg-[#f3332f] px-5 py-5 text-white">
-            OUR MISSION
-          </button>
+          
 
-          <p className="mx-[60px] text-center pt-5">
-            Bonk 2.0 is dedicated to becoming the premier Web3 community on the
-            Solana blockchain. Our mission is to foster inclusivity, innovation,
-            and decentralized collaboration. We aim to create a vibrant space
-            where creators, developers, and enthusiasts converge to push the
-            boundaries of what's possible. Through transparent governance and
-            community-driven initiatives, Bonk 2.0 seeks to set the standard for
-            excellence in the Solana ecosystem. We are committed to providing a
-            dynamic platform that empowers individuals, fosters creativity, and
-            propels us to be the best Web3 community on the Solana chain. Join
-            us in shaping the future of decentralized collaboration.
-          </p>
+          
         </div>
       </div>
 
-      {/* third section */}
-      <div className="bg-[#f3332f] text-center text-white py-[100px]">
-        <button className="bg-white  px-5 py-5 text-[#f3332f]">
-          TOKENOMICS
-        </button>
+      
 
-        <div className="flex space-x-8 justify-center pt-[30px]">
-          <div>
-            <p>100,000,000</p>
-            <p>Total Supply</p>
-          </div>
-
-          <div>
-            <p>0/0</p>
-            <p>Tax</p>
-          </div>
-        </div>
-      </div>
-
-      {/* forth section */}
-      <div className="bg-white  text-[#f3332f] py-[120px]">
-        <div className="grid justify-items-center">
-          <button className="bg-[#f3332f]  px-5 py-5 text-white">
-            ROADMAP
-          </button>
-        </div>
-
-        <div>
-          {/* content 1 */}
-          <div className="px-[60px] sm:mx-[100px] pt-[30px] text-balance justify-items-center grid  grid-cols-2 sm:grid sm:grid-cols-4 space-y-[30px] sm:space-y-[0px] space-x-[20px]  sm:space-x-[70px]">
-            <div className="pt-[30px] sm:pt-[0px] pl-[20px] sm:pl-[20px]">
-              <h2>PHASE 1</h2>
-
-              <ul className="list-disc text-[#524c47]">
-                <li> Token launch</li>
-                <li> Website and social medias</li>
-                <li>Bonk meme generator</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2>PHASE 1</h2>
-
-              <ul className="list-disc text-[#524c47]">
-                <li> Token launch</li>
-                <li> Website and social medias</li>
-                <li>Bonk meme generator</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2>PHASE 1</h2>
-
-              <ul className="list-disc text-[#524c47]">
-                <li> Token launch</li>
-                <li> Website and social medias</li>
-                <li>Bonk meme generator</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2>PHASE 1</h2>
-
-              <ul className="list-disc text-[#524c47]">
-                <li> Token launch</li>
-                <li> Website and social medias</li>
-                <li>Bonk meme generator</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </main>
   );
 }
